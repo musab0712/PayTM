064bcd85a7e40e9cf846bb998363324a3dd5d606
Musab Hassan <musabhassan1999@gmail.com>
2024-03-24 21:14:14 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
-const mainRouter = require("./routes/index");
+const mainRouters = require("./routes/index");
 const app = express();
 
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1", mainRouter);
+app.use("/api/v1", mainRouters);
 
 app.listen(port, () => {
   console.log(`listening on the port ${port}`);
